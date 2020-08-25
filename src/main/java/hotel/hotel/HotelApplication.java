a488ce9f9f6a45f4ef5a2885fb5a5f1319e4addf
bouyou <billycobam@hotmail.fr>
2020-08-25 21:32:07 +0200

    null pointer

--- src/main/java/hotel/hotel/HotelApplication.java
+++ src/main/java/hotel/hotel/HotelApplication.java
@@ -2,10 +2,9 @@ package hotel.hotel;
 
 import hotel.hotel.entities.Chambre;
 import hotel.hotel.entities.ChambreRepository;
-import org.hibernate.service.spi.InjectService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 import java.util.List;
@@ -14,14 +13,13 @@ import java.util.List;
 @SpringBootApplication
 public class HotelApplication {
 
-	@Autowired
-	private static ChambreRepository repo;
-
-
 	public static void main(String[] args) {
 
 		System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(HotelApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(HotelApplication.class, args);
+		ChambreRepository repo = context.getBean(ChambreRepository.class);
+
 		List<Chambre> toto = repo.findAll();
 		System.out.println("hello");
 
