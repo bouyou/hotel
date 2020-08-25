ea233810c8bcbfc94f3719032adc6035b935b1f4
bouyou <billycobam@hotmail.fr>
2020-08-25 19:36:56 +0200

    compile + jpa + lombok

---
+++ src/main/java/hotel/hotel/entities/Chambre.java
@@ -0,0 +1,36 @@
+package hotel.hotel.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "chambre")
+@Data
+public class Chambre implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "numero", nullable = false)
+    private String numero;
+
+    @Column(name = "nbPax", nullable = false)
+    private Integer nbPax;
+
+    @Column(name = "dateModif", nullable = false)
+    private Date dateModif;
+
+    @Column(name = "dateDelete")
+    private Date dateDelete;
+
+    @Column(name = "id_hotel", nullable = false)
+    private Integer idHotel;
+
+}
