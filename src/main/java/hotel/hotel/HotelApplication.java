c1993cbb31d4ad557c9e9fb5bd7672c6df6c07c1
bouyou <billycobam@hotmail.fr>
2020-08-25 18:52:00 +0200

    compile en mode spring boot
    run en mode application

--- src/main/java/hotel/hotel/HotelApplication.java
+++ src/main/java/hotel/hotel/HotelApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HotelApplication.class, args);
+
+		System.out.println("hello");
+
 	}
+
 
 }
