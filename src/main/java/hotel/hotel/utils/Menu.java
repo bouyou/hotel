29b5770f708afaadc1e8193680f4e5654539d0f8
admin <rocketyen@gmail.com>
2020-08-26 16:33:28 +0200

    add back to main and leave app

--- src/main/java/hotel/hotel/utils/Menu.java
+++ src/main/java/hotel/hotel/utils/Menu.java
@@ -6,11 +6,10 @@ public class Menu<Static> {
     public static Scanner clavier = new Scanner(System.in);
     public static Integer choix;
 
-    public static  void showMainMenu(){
-        //1 Gestion Client
-        //2 Gestion hotel
-        //3 Gestion Chambre
+    public static void showMainMenu(){
+        System.out.println("---- BIENVENUE ----");
         System.out.println("Menu Principal : ");
+
         do {
             System.out.println("Sommaire");
             System.out.println("1 : Gestion Clients");
@@ -25,15 +24,18 @@ public class Menu<Static> {
                     break;
                 case 2 : Menu.showGestionHotel();
                     break;
-                //case 3 : Menu.showGestionChambre;
-                    //break;
-                //case 4 : Menu.showGestionReservation;
-                    //break;
+                case 3 : Menu.showGestionChambre();
+                    break;
+                case 4 : Menu.showGestionReservation();
+                    break;
+                case 0 : Menu.showQuitter();
+                    break;
             }
             break;
         }
         while(choix!=0);
     }
+
     private static void showGestionClient() {
         System.out.println("je suis dans menu Gestion Client");
         do {
@@ -53,10 +55,13 @@ public class Menu<Static> {
                     break;
                 case 4 : Menu.showMainMenu();
                     break;
+                case 0 : Menu.showQuitter();
+                    break;
             }
         }
         while(choix!=0);
     }
+
     private static void showAjoutClient() {
         System.out.println("je suis dans ajouter un Client");
     }
@@ -88,10 +93,13 @@ public class Menu<Static> {
                     break;
                 case 4 : Menu.showMainMenu();
                     break;
+                case 0 : Menu.showQuitter();
+                    break;
             }
         }
         while(choix!=0);
     }
+
     private static void showAjoutHotel() {
         System.out.println("je suis dansajouter un Client");
     }
@@ -101,26 +109,80 @@ public class Menu<Static> {
     private static void showSupprHotel() {
         System.out.println("je suis dans supprimer un Client");
     }
-    /*private static void showGestionChambre() {
+
+    private static void showGestionChambre() {
         System.out.println("je suis dans menu Gestion Chambre");
         do {
-            System.out.println("1 : Ajouter un client");
-            System.out.println("2 : Modifier un Client");
-            System.out.println("2 : Supprimer un Client");
+            System.out.println("1 : Ajouter une Chambre");
+            System.out.println("2 : Modifier une Chambre");
+            System.out.println("3 : Supprimer une Chambre");
+            System.out.println("4 : Retour au menu principal");
             System.out.println("0 : Quitter ");
             System.out.println("Choix : ");
             choix = clavier.nextInt();
             switch(choix){
-                case 1 : Menu.showAjoutClient();
+                case 1 : Menu.showAjoutChambre();
                     break;
-                case 2 : Menu.showModifClient();
+                case 2 : Menu.showModifChambre();
                     break;
-                case 3 : Menu.showSupprClient();
+                case 3 : Menu.showSupprChambre();
+                    break;
+                case 4 : Menu.showMainMenu();
+                    break;
+                case 0 : Menu.showQuitter();
                     break;
             }
         }
         while(choix!=0);
-    }*/
+    }
+    private static void showAjoutChambre() {
+        System.out.println("je suis dans ajouter une Chambre");
+    }
+    private static void showModifChambre() {
+        System.out.println("je suis dans modification d'une Chambre");
+    }
+    private static void showSupprChambre() {
+        System.out.println("je suis dans supprimer une Chambre");
+    }
+
+    private static void showGestionReservation() {
+        System.out.println("je suis dans menu Gestion Reservation");
+        do {
+            System.out.println("1 : Ajouter une Reservation");
+            System.out.println("2 : Modifier une Reservation");
+            System.out.println("3 : Supprimer une Reservation");
+            System.out.println("4 : Retour au menu principal");
+            System.out.println("0 : Quitter ");
+            System.out.println("Choix : ");
+            choix = clavier.nextInt();
+            switch(choix){
+                case 1 : Menu.showAjoutReservation();
+                    break;
+                case 2 : Menu.showModifReservation();
+                    break;
+                case 3 : Menu.showSupprReservation();
+                    break;
+                case 4 : Menu.showMainMenu();
+                    break;
+                case 0 : Menu.showQuitter();
+                    break;
+            }
+        }
+        while(choix!=0);
+    }
+    private static void showAjoutReservation() {
+        System.out.println("je suis dans ajouter une Reservation");
+    }
+    private static void showModifReservation() {
+        System.out.println("je suis dans modification d'une Reservation");
+    }
+    private static void showSupprReservation() {
+        System.out.println("je suis dans supprimer une Reservation");
+    }
+
+    private static void showQuitter() {
+        System.out.println("---- A BIENTOT ----");
+    }
 }
 
 
